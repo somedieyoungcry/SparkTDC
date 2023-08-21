a169348f4271aea15d159607b07cf80fd0dffb6b
Some Die Young <79615792+somedieyoungcry@users.noreply.github.com>
2023-08-21 14:20:32 -0600

    Add constants

--- main.py
+++ main.py
@@ -5,50 +5,47 @@ from trys.transformations.transformations import Transformations
 
 
 def main():
-    spark = SparkSession.builder.appName("sparkMain") \
-        .master("local[*]") \
-        .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
+    spark = SparkSession.builder.appName(c.APP_NAME) \
+        .master(c.MODE) \
+        .config(c.POLICY, c.LEGACY) \
         .getOrCreate()
 
     t = Transformations()
 
-    policy_df = spark.read.parquet(
-        "resources/cuttofdate=2023-08-16_2000_3A00_3A00/part-00000-14be24e8-07ac-45eb-a56f-6fb1e56618ec.c000.snappy.parquet")
-    clients_df = spark.read.parquet(
-        "resources/user_charge_date=2023-08-17/part-00000-cd81d3c7-8ee7-44f5-8c28-91009fc70e91.c000.snappy.parquet")
-    cars_df = spark.read.parquet("resources/CARS/part-00000-5c2c513b-9db5-4736-9e00-b2604c4f9ea4-c000.snappy.parquet")
-    incidents_df = spark.read.parquet(
-        "resources/load_date=2023-08-17/part-00000-29af0d9c-ea76-4b7a-8e51-ffbfa3a592cb.c000.snappy.parquet")
+    policy_df = spark.read.parquet(c.POLICY_PARQUET)
+    clients_df = spark.read.parquet(c.CLIENTS_PARQUET)
+    cars_df = spark.read.parquet(c.CARS_PARQUET)
+    incidents_df = spark.read.parquet(c.INCIDENTS_PARQUET)
 
-    policy_df.printSchema()
-    clients_df.printSchema()
-    cars_df.printSchema()
-    incidents_df.printSchema()
+    # policy_df.printSchema()
+    # clients_df.printSchema()
+    # cars_df.printSchema()
+    # incidents_df.printSchema()
 
 
     print("Hacemos el join")
     creditos_output = t.joined_dfs(policy_df, clients_df, cars_df, incidents_df)
     # creditos_output.printSchema()
-    # creditos_output.show()
+    creditos_output.show()
 
     print("Funcion rank 100")
     rankTop = t.rank_incidents(creditos_output)
-    rankTop_null = rankTop.na.drop(subset=["id_car_brand"])
+    rankTop_null = rankTop.na.drop(subset=[c.CARID])
     rankTop_null.show()
 
     print("Rank por colores")
     rankColor = t.rank_colors(creditos_output)
-    rankColor_null = rankColor.na.drop(subset=["car_color"])
+    rankColor_null = rankColor.na.drop(subset=[c.CAR_COLOR])
     rankColor_null.show()
 
     print("Rank por paises")
     rankCountry = t.rank_countrys(creditos_output)
-    rankCountry_null = rankCountry.na.drop(subset=["country_name"])
+    rankCountry_null = rankCountry.na.drop(subset=[c.COUNTRY_NAME])
     rankCountry_null.show()
 
     print("Rank por edades")
     rankAge = t.top10_ages(creditos_output)
-    rankAge_null = rankAge.na.drop(subset=["user_age"])
+    rankAge_null = rankAge.na.drop(subset=[c.USER_AGE])
     rankAge_null.show()
 
     print("Clasificacion")
--- trys/transformations/transformations.py
+++ trys/transformations/transformations.py
@@ -15,90 +15,72 @@ class Transformations:
         joined_df2 = joined_df1.join(cars_df, policy_df[c.CARID] == cars_df[c.BRANDID], c.JOIN1)
         joined_df3 = joined_df2.join(incidents_df, policy_df[c.IDPOLICY] == incidents_df[c.IDPOL], c.JOIN2)
 
-        """selected_fields = [
-            policy_df["id_policy"],
-            policy_df["id_client"],
-            clients_df["user_email"],
-            policy_df["id_car_brand"],
-            cars_df["brand_name"],
-            cars_df["country_name"],
-            policy_df["car_color"],
-            policy_df["sum_assured"],
-            policy_df["car_model"],
-            policy_df["end_date_validity"],
-            clients_df["user_age"],
-            incidents_df["incident_id"],
-            incidents_df["repair_amount"]
-        ]
-
-        selected_df = joined_df3.select(*selected_fields)"""
-
         return joined_df3
 
     @staticmethod
     def rank_incidents(df: DataFrame) -> DataFrame:
-        count_incidents = df.groupBy(c.CARID).agg(f.count("*").alias(c.ACCIDENTCOUNT))
-        window_spec = Window.orderBy(f.col(c.ACCIDENTCOUNT).desc())
+        count_incidents = df.groupBy(c.CARID).agg(f.count("*").alias(c.ACCIDENT_COUNT))
+        window_spec = Window.orderBy(f.col(c.ACCIDENT_COUNT).desc())
         rank_incidents = count_incidents.withColumn(c.RANK, f.row_number().over(window_spec) - 1)
 
         return rank_incidents
 
     @staticmethod
     def rank_colors(df: DataFrame) -> DataFrame:
-        count_colors = df.groupBy("car_color").agg(f.count("*").alias("accident_count"))
-        window_func = Window.orderBy(f.col("accident_count").desc())
-        rank_colors = count_colors.withColumn("rank", f.row_number().over(window_func) - 1)
+        count_colors = df.groupBy(c.CAR_COLOR).agg(f.count("*").alias(c.ACCIDENT_COUNT))
+        window_func = Window.orderBy(f.col(c.ACCIDENT_COUNT).desc())
+        rank_colors = count_colors.withColumn(c.RANK, f.row_number().over(window_func) - 1)
 
         return rank_colors
 
     @staticmethod
     def rank_countrys(df: DataFrame) -> DataFrame:
-        count_countries = df.groupBy("country_name").agg(f.count("*").alias("accident_count"))
-        window_spec = Window.orderBy(f.col("accident_count").desc())
-        rank_countries = count_countries.withColumn("rank", f.row_number().over(window_spec) - 1)
+        count_countries = df.groupBy(c.COUNTRY_NAME).agg(f.count("*").alias(c.ACCIDENT_COUNT))
+        window_spec = Window.orderBy(f.col(c.ACCIDENT_COUNT).desc())
+        rank_countries = count_countries.withColumn(c.RANK, f.row_number().over(window_spec) - 1)
 
         return rank_countries
 
     @staticmethod
     def top10_ages(df: DataFrame) -> DataFrame:
-        age_accident_count = df.groupBy("user_age").agg(f.count("*").alias("accident_count"))
-        window_spec = Window.orderBy(f.col("accident_count").desc())
-        rank_ages = age_accident_count.withColumn("rank", f.row_number().over(window_spec) - 1)
+        age_accident_count = df.groupBy(c.USER_AGE).agg(f.count("*").alias(c.ACCIDENT_COUNT))
+        window_spec = Window.orderBy(f.col(c.ACCIDENT_COUNT).desc())
+        rank_ages = age_accident_count.withColumn(c.RANK, f.row_number().over(window_spec) - 1)
         top_10_ages = rank_ages.filter(rank_ages.rank <= 10)
 
         return top_10_ages
 
     def update_clasification(self, df: DataFrame) -> DataFrame:
         color_rank_df = self.rank_colors(df)
-        max_accident_color = color_rank_df.select("car_color").first()["car_color"]
+        max_accident_color = color_rank_df.select(c.CAR_COLOR).first()[c.CAR_COLOR]
         country_rank_df = self.rank_countrys(df)
-        max_accident_country = country_rank_df.select("country_name").first()["country_name"]
+        max_accident_country = country_rank_df.select(c.COUNTRY_NAME).first()[c.COUNTRY_NAME]
         top_10_ages_df = self.top10_ages(df)
-        avg_age = top_10_ages_df.select(f.avg("user_age")).first()[0]
+        avg_age = top_10_ages_df.select(f.avg(c.USER_AGE)).first()[0]
         incidents_rank_df = self.rank_incidents(df)
-        incidents_rank_df = incidents_rank_df.withColumnRenamed("id_car_brand",
-                                                                "rank_id_car_brand")
-        df_with_incidents = df.join(incidents_rank_df, df["id_car_brand"] == incidents_rank_df["rank_id_car_brand"],
-                                    "left_outer")
-        updated_df = df_with_incidents.withColumn("points",
-                                                  f.when(df_with_incidents["velocity_max_amount"] >= 200, 2).otherwise(
+        incidents_rank_df = incidents_rank_df.withColumnRenamed(c.CARID,
+                                                                c.RANK_CAR_BRAND)
+        df_with_incidents = df.join(incidents_rank_df, df[c.CARID] == incidents_rank_df[c.RANK_CAR_BRAND],
+                                    c.JOIN3)
+        updated_df = df_with_incidents.withColumn(c.POINTS,
+                                                  f.when(df_with_incidents[c.VELOCITY_MAX] >= 200, 2).otherwise(
                                                       0) +
-                                                  f.when(df_with_incidents["user_age"] > avg_age, 3).otherwise(0) +
-                                                  f.when(df_with_incidents["car_color"] == max_accident_color,
+                                                  f.when(df_with_incidents[c.USER_AGE] > avg_age, 3).otherwise(0) +
+                                                  f.when(df_with_incidents[c.CAR_COLOR] == max_accident_color,
                                                          1).otherwise(0) +
-                                                  f.when(df_with_incidents["country_name"] == max_accident_country,
+                                                  f.when(df_with_incidents[c.COUNTRY_NAME] == max_accident_country,
                                                          3).otherwise(0) +
-                                                  f.when(df_with_incidents["accident_count"] ==
-                                                         df_with_incidents.select(f.max("accident_count")).first()[0],
+                                                  f.when(df_with_incidents[c.ACCIDENT_COUNT] ==
+                                                         df_with_incidents.select(f.max(c.ACCIDENT_COUNT)).first()[0],
                                                          2).otherwise(0)
                                                   )
 
-        updated_df = updated_df.withColumn("classification",
-                                           f.when(updated_df["points"] <= 5, "C").otherwise(
-                                               f.when(updated_df["points"] >= 9, "A").otherwise("B")
+        updated_df = updated_df.withColumn(c.CLASSIFICATION,
+                                           f.when(updated_df[c.POINTS] <= 5, "C").otherwise(
+                                               f.when(updated_df[c.POINTS] >= 9, "A").otherwise("B")
                                            )
                                            )
 
-        updated_df = updated_df.na.drop("all")
+        updated_df = updated_df.na.drop(c.ALL)
 
         return updated_df
